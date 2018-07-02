867a91889ca1302ca9de2f289b2fabd669f1ea83
KSSHIN <sksbest4@gmail.com>
2018-07-02 13:29:56 +0900

    RestAPI 호출을 axios로 변경후 호출 부분 수정중

--- src/main/js/components/MeasuresHistory.js
+++ src/main/js/components/MeasuresHistory.js
@@ -8,32 +8,36 @@ import ReactTooltip from 'react-tooltip'
 import axios from 'axios';
 
 export default class MeasuresHistory extends React.PureComponent {
-    constructor(props){
+    constructor(props) {
         super();
-        this.IssueToRedmine=this.IssueToRedmine.bind(this);
+        this.IssueToRedmine = this.IssueToRedmine.bind(this);
     }
 
-    IssueToRedmine(){
+    IssueToRedmine() {
         axios.get('/api/settings/values?keys=sonar.redmine.hosturl,sonar.redmine.api-access-key')
-            .then(function (sonarPredmine){
-                const sonarkeylength=sonarPredmine.settings.length;
-                for(let i=0; i< sonarkeylength; i++){
-                    if(sonarPredmine.settings.keys[i]==='sonar.redmine.hosturl'){
-                        var url=sonarPredmine.settings.keys[i].value;
+            .then(function (sonarPredmine) {
+                const sonarkeylength = sonarPredmine.data.settings.length;
+                for (let i = 0; i < sonarkeylength; i++) {
+                    if (sonarPredmine.data.settings[i].key === 'sonar.redmine.hosturl') {
+                        var url = sonarPredmine.data.settings[i].value;
+                        console.log(url);
                     }
-                    else{
-                        var acc=sonarPredmine.settings.keys[i].value;
+                    else {
+                        var acc = sonarPredmine.data.settings[i].value;
+                        console.log(acc);
                     }
                 }
                 axios({
-                    method:'get',
-                    url: url+'/issues.json',
-                    headers:{
+/*                    proxy: {host:url,port:'80'},*/
+                    adapter: 'axios-jsonp',
+                    method: 'get',
+                    url: url + '/issues.json',
+                    headers: {
                         'X-Redmine-API-KEY': acc,
                         'Context-Type': 'application/json'
                     }
-                }).then(function(result){
-                   console.log(result)
+                }).then(function (restRedmine) {
+                    console.log(restRedmine)
                 });
             });
     }
@@ -61,7 +65,9 @@ export default class MeasuresHistory extends React.PureComponent {
                 <td className="thin nowrap text-left">
                     <div>
                         <span><a data-tip data-for="toggler">{this.simplification(this.props.issue.message, 20)}</a>
-                            <ReactTooltip id="toggler" getContent={[() => {return this.props.issue.message}]}></ReactTooltip>
+                            <ReactTooltip id="toggler" getContent={[() => {
+                                return this.props.issue.message
+                            }]}></ReactTooltip>
                         </span>
                     </div>
                 </td>
