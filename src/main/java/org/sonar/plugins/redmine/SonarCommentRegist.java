45bfdfc7bba91fae131e4d5b54c09bef5a1be7ef
KSSHIN <sksbest4@gmail.com>
2018-09-03 18:00:23 +0900

    Type 에 따라 자동으로 Redmine으로 보낼예정

---
+++ src/main/java/org/sonar/plugins/redmine/SonarCommentRegist.java
@@ -0,0 +1,9 @@
+package org.sonar.plugins.redmine;
+
+public class SonarCommentRegist  {
+    public SonarCommentRegist () {
+    }
+    public void sonarconnect(String baseURL, String key, String redmineURL, int issueId){
+
+    }
+}
--- src/main/java/org/sonar/plugins/redmine/computerEngine/SonarqubeIssueInScannerPostJob.java
+++ src/main/java/org/sonar/plugins/redmine/computerEngine/SonarqubeIssueInScannerPostJob.java
@@ -11,9 +11,11 @@ import org.sonar.api.batch.postjob.PostJobContext;
 import org.sonar.api.batch.postjob.PostJobDescriptor;
 import org.sonar.api.batch.postjob.issue.PostJobIssue;
 import org.sonar.api.config.Configuration;
+import org.sonar.api.rules.RuleType;
+import org.sonar.plugins.redmine.SonarCommentRegist;
 import org.sonar.plugins.redmine.config.RedmineSettingsConfiguration;
-
 import java.util.Objects;
+
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
@@ -21,10 +23,13 @@ public class SonarqubeIssueInScannerPostJob implements PostJob {
 
     private static final Logger LOGGER = Loggers.get(SonarqubeIssueInScannerPostJob.class);
     private RedmineSettingsConfiguration redmineSettingsConfiguration;
+    private SonarCommentRegist sonarCommentRegist;
 
     public SonarqubeIssueInScannerPostJob(Configuration configuration) {
         RedmineSettingsConfiguration redmineSettingsConfiguration = new RedmineSettingsConfiguration(configuration);
         this.redmineSettingsConfiguration = redmineSettingsConfiguration;
+        SonarCommentRegist sonarCommentRegist=new SonarCommentRegist();
+        this.sonarCommentRegist=sonarCommentRegist;
     }
 
     @Override
@@ -40,13 +45,19 @@ public class SonarqubeIssueInScannerPostJob implements PostJob {
             if (redmineSettingsConfiguration.Auto_regist()) {
                 for (PostJobIssue issue : context.issues()) {
                     if (redmineSettingsConfiguration.Bug()) {
-                        connectRedmineIssue(issue);
+                        if (RuleType.BUG.toString().equals(redmineSettingsConfiguration.Bug())) {
+                            connectRedmineIssue(issue);
+                        }
                     }
                     if (redmineSettingsConfiguration.Code_Smell()) {
-                        connectRedmineIssue(issue);
+                        if (RuleType.CODE_SMELL.toString().equals(redmineSettingsConfiguration.Code_Smell())) {
+                            connectRedmineIssue(issue);
+                        }
                     }
                     if (redmineSettingsConfiguration.VULNERABILITY()) {
-                        connectRedmineIssue(issue);
+                        if (RuleType.VULNERABILITY.toString().equals(redmineSettingsConfiguration.VULNERABILITY())) {
+                            connectRedmineIssue(issue);
+                        }
                     }
                 }
             }
@@ -54,50 +65,51 @@ public class SonarqubeIssueInScannerPostJob implements PostJob {
     }
 
     private void connectRedmineIssue(PostJobIssue issue) {
+        LOGGER.debug("AUTO_SEVERITY_LEVEL", redmineSettingsConfiguration.AUTO_SEVERITY_LEVEL(redmineSettingsConfiguration.AUTO_SEVERITY()));
         for (int i = 0; i < redmineSettingsConfiguration.AUTO_SEVERITY_LEVEL(redmineSettingsConfiguration.AUTO_SEVERITY()); i++) {
-            String key = issue.key();
-            String ruleKey = issue.ruleKey().toString();
-            String component = issue.componentKey();
-            Integer line = issue.line();
-            String message = issue.message();
-            String severity = null;
             if (issue.severity().toString().equals(redmineSettingsConfiguration.AUTO_SEVERITY())) {
-                severity = issue.severity().toString();
-            }
-            LOGGER.debug("OPEN {} : {}({})", ruleKey, issue.componentKey(), line);
-            if (redmineSettingsConfiguration.RedmineURL() != null && redmineSettingsConfiguration.APIKey() != null && redmineSettingsConfiguration.ProjectKey() != null && redmineSettingsConfiguration.TrackerId() != null && redmineSettingsConfiguration.UserId() != null) {
-                mgr = RedmineManagerFactory.createWithApiKey(Objects.requireNonNull(redmineSettingsConfiguration.RedmineURL()), redmineSettingsConfiguration.APIKey());
-                IssueManager issueMgr = mgr.getIssueManager();
-                Issue redmineIssue = IssueFactory.create(null);
-                try {
-                    setProjectInfo(redmineIssue, redmineSettingsConfiguration.ProjectKey());
-                    setTrackerInfo(redmineIssue, redmineSettingsConfiguration.TrackerId());
-                    setUserInfo(redmineIssue, redmineSettingsConfiguration.UserId());
-                    redmineIssue.setSubject(ruleKey);
-                    redmineIssue.setDescription(ruleKey + '\n' + message + "\n\n" + "\n\n Source Code location:\n" + component + " Line : " + line + "\n\n" + "check the sonarqube \n<" + redmineSettingsConfiguration.baseUrl() + "/project/issues?id=" + "&open=" + key + "&severities=" + severity);
-                    Issue newIssue = issueMgr.createIssue(redmineIssue);
-                    issueMgr.update(newIssue);
-                } catch (RedmineException e) {
-                    e.printStackTrace();
+                String key = issue.key();
+                String ruleKey = issue.ruleKey().toString();
+                String component = issue.componentKey();
+                Integer line = issue.line();
+                String message = issue.message();
+                String severity = issue.severity().toString();
+                LOGGER.debug("OPEN {} : {}({})", ruleKey, issue.componentKey(), line);
+                if (redmineSettingsConfiguration.RedmineURL() != null && redmineSettingsConfiguration.APIKey() != null && redmineSettingsConfiguration.ProjectKey() != null && redmineSettingsConfiguration.TrackerId() != null && redmineSettingsConfiguration.UserId() != null) {
+                    mgr = RedmineManagerFactory.createWithApiKey(Objects.requireNonNull(redmineSettingsConfiguration.RedmineURL()), redmineSettingsConfiguration.APIKey());
+                    IssueManager issueMgr = mgr.getIssueManager();
+                    Issue redmineIssue = IssueFactory.create(null);
+                    try {
+                        setProjectInfo(redmineIssue, redmineSettingsConfiguration.ProjectKey());
+                        setTrackerInfo(redmineIssue, redmineSettingsConfiguration.TrackerId());
+                        setUserInfo(redmineIssue, redmineSettingsConfiguration.UserId());
+                        redmineIssue.setSubject(ruleKey);
+                        redmineIssue.setDescription(ruleKey + '\n' + message + "\n\n" + "\n\n Source Code location:\n" + component + " Line : " + line + "\n\n" + "check the sonarqube \n<" + redmineSettingsConfiguration.baseUrl() + "/project/issues?id=" + "&open=" + key + "&severities=" + severity);
+                        Issue newIssue = issueMgr.createIssue(redmineIssue);
+                        issueMgr.update(newIssue);
+                        LOGGER.debug("newIssue.getId()", newIssue.getId());
+                        sonarCommentRegist.sonarconnect(redmineSettingsConfiguration.baseUrl(),key,redmineSettingsConfiguration.RedmineURL(),newIssue.getId());
+                    } catch (RedmineException e) {
+                        e.printStackTrace();
+                    }
                 }
-
             }
         }
     }
 
-    private void setProjectInfo(Issue issue, String projectKey) throws RedmineException {
-        int intProjectKey = mgr.getProjectManager().getProjectByKey(projectKey).getId();
-        Project project = ProjectFactory.create(intProjectKey);
-        issue.setProject(project);
-    }
+        private void setProjectInfo (Issue issue, String projectKey) throws RedmineException {
+            int intProjectKey = mgr.getProjectManager().getProjectByKey(projectKey).getId();
+            Project project = ProjectFactory.create(intProjectKey);
+            issue.setProject(project);
+        }
 
-    private void setTrackerInfo(Issue issue, String trackerId) {
-        Tracker tracker = TrackerFactory.create(Integer.parseInt(trackerId));
-        issue.setTracker(tracker);
-    }
+        private void setTrackerInfo (Issue issue, String trackerId){
+            Tracker tracker = TrackerFactory.create(Integer.parseInt(trackerId));
+            issue.setTracker(tracker);
+        }
 
-    private void setUserInfo(Issue issue, String UserId) {
-        User user = UserFactory.create(Integer.valueOf(UserId));
-        issue.setAssignee(user);
+        private void setUserInfo (Issue issue, String UserId){
+            User user = UserFactory.create(Integer.valueOf(UserId));
+            issue.setAssignee(user);
+        }
     }
-}
